c96794c0144fbba8b8c1ae8680b76e577a2ac527
Vudvord387 <romjuk321@gmail.com>
2023-04-15 18:37:14 +0300

    CardBack.InProcess

--- Assets/Scripts/CardInfoScr.cs
+++ Assets/Scripts/CardInfoScr.cs
@@ -8,24 +8,30 @@ public class CardInfoScr : MonoBehaviour
 {
     public Card SelfCard;
     public Image Logo;
-    public TextMeshProUGUI Name;
+    public TextMeshProUGUI Name, Attack, Defense;
+    public GameObject HideObj;
 
     public void HideCardInfo(Card card)
     {
-        /*SelfCard = card;
-        Logo.sprite = null;
-        Name.text = "";*/
-        ShowCardInfo(card);
+       SelfCard = card;
+       HideObj.SetActive(true);
+       
     }
 
     public void ShowCardInfo(Card card)
     {
+        HideObj.SetActive(false);
         SelfCard = card;
 
         Logo.sprite = card.Logo;
         Logo.preserveAspect = true;
         Name.text = card.Name;
+        
+        Attack.text = SelfCard.Attack.ToString();
+        Defense.text = SelfCard.Defense.ToString();
+    
     }
+
    private void Start() 
    {
         //ShowCardInfo(CardManager.AllCards[transform.GetSiblingIndex()]);     
