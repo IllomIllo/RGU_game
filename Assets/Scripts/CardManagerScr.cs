a42c258da6771f0f5086188eef9dc26a2b30e1e2
Vudvord387 <mr.bzd98@mail.ru>
2023-04-26 15:00:09 +0300

    4-й видос полностью

--- Assets/Scripts/CardManagerScr.cs
+++ Assets/Scripts/CardManagerScr.cs
@@ -49,12 +49,7 @@ public class CardManagerScr : MonoBehaviour
     // список карт
     public void Awake()
     {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-<<<<<<< Updated upstream
+
         CardManager.AllCards.Add(new Card("LD","Sprites/Cards/LD", 5, 4, 4));
         CardManager.AllCards.Add(new Card("Brother","Sprites/Cards/Brother", 5, 5, 3));
         CardManager.AllCards.Add(new Card("Decan","Sprites/Cards/Decan", 9, 5, 7));
@@ -62,35 +57,7 @@ public class CardManagerScr : MonoBehaviour
         CardManager.AllCards.Add(new Card("Starosta","Sprites/Cards/Starosta", 2, 14, 5));
         CardManager.AllCards.Add(new Card("Starshecursnik","Sprites/Cards/Starshecursnik", 1, 1, 1));
         CardManager.AllCards.Add(new Card("Laborant","Sprites/Cards/Laborant", 2, 4, 2));
-=======
-        CardManager.AllCards.Add(new Card("Лучший друг","Sprites/StudentCards/LD", 5, 4));
-        CardManager.AllCards.Add(new Card("Брат", "Sprites/StudentCards/Brother", 5, 5));
-        CardManager.AllCards.Add(new Card("Decan","Sprites/Cards/Decan", 5, 3));
-        CardManager.AllCards.Add(new Card("Отличник", "Sprites/StudentCards/Otlichnik", 10, 5));
-        CardManager.AllCards.Add(new Card("Староста", "Sprites/StudentCards/Starosta", 2, 14));
-        CardManager.AllCards.Add(new Card("Старшекурсник", "Sprites/StudentCards/Starshecursnik", 1, 1));
-        CardManager.AllCards.Add(new Card("Лаборант","Sprites/Cards/Laborant", 2, 4));
->>>>>>> Stashed changes
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> parent of 3002ef5 (Половина 4-го видоса)
-=======
->>>>>>> Stashed changes
-        CardManager.AllCards.Add(new Card("LD","Sprites/Cards/LD", 5, 4));
-        CardManager.AllCards.Add(new Card("Brother","Sprites/Cards/Brother", 5, 5));
-        CardManager.AllCards.Add(new Card("Decan","Sprites/Cards/Decan", 5, 3));
-        CardManager.AllCards.Add(new Card("Otlichnik","Sprites/Cards/Otlichnik", 10, 5));
-        CardManager.AllCards.Add(new Card("Starosta","Sprites/Cards/Starosta", 2, 14));
-        CardManager.AllCards.Add(new Card("Starshecursnik","Sprites/Cards/Starshecursnik", 1, 1));
-        CardManager.AllCards.Add(new Card("Laborant","Sprites/Cards/Laborant", 2, 4));
-<<<<<<< Updated upstream
-<<<<<<< HEAD
->>>>>>> parent of 3002ef5 (Половина 4-го видоса)
-=======
-=======
->>>>>>> Stashed changes
->>>>>>> parent of 3002ef5 (Половина 4-го видоса)
+
     }
 }
    
