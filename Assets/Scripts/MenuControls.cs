50c6a74c9bd7b4bd23974b27f16f78bc61f3a122
neumnaya <vova.vova999999@mail.ru>
2023-05-10 01:13:40 +0300

    вид настроек+мэйн менюшки
    мой креатив покинул чат, поэтому пока что так

--- Assets/Scripts/MenuControls.cs
+++ Assets/Scripts/MenuControls.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour
-{   
-    
+{
     public void PlayPressed()
     {
         SceneManager.LoadScene("Game");
     }
-    
+
     public void SettingsPressed()
     {
-      SceneManager.LoadScene("Settings");
-        
+        SceneManager.LoadScene("Settings");
+
     }
 
-     public void ExitPressed()
+    public void ExitPressed()
     {
         Application.Quit();
         Debug.Log("Exit pressed!");
     }
-
 
 }
